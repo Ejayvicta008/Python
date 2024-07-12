f3df92b2a1dea90f347b1f3f731354aa1a2727e9
Eryl John <128870456+Ejayvicta008@users.noreply.github.com>
2024-07-13 02:41:24 +0800

    Add files via upload

---
+++ LifeInWeeks.py
@@ -0,0 +1,6 @@
+
+#This calculator will calculate how many weeks you still have to live considering you will die at 90 years old.
+age = input()
+Years = 90 - int(age)
+weeks_left = 52 * (Years)
+print(f"You have " + weeks_left + " weeks left")
